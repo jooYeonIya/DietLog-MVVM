c268b757dac651416e0311a16538e18c858e1a09
jooYeon <jooyeonstudyios@gmail.com>
2024-04-24 18:33:05 +0900

    design: CalendarView 추가 (#3)

--- DietLog-MVVM/View/MyInfo/MyInfoViewController.swift
+++ DietLog-MVVM/View/MyInfo/MyInfoViewController.swift
@@ -16,6 +16,8 @@ class MyInfoViewController: BaseViewController {
     
     // MARK: - Component
     private lazy var welcomLabel = UILabel()
+    private lazy var calendarView = FSCalendar()
+    private lazy var calendarBackgroundView = UIView()
     
     // MARK: - 변수
     private let nickname: String = "nickname"
@@ -28,8 +30,13 @@ class MyInfoViewController: BaseViewController {
     
     // MARK: - Setup UI
     override func setupUI() {
-        view.addSubviews([welcomLabel])
+        view.addSubviews([welcomLabel,
+                          calendarBackgroundView])
+        
+        calendarBackgroundView.addSubview(calendarView)
+        
         setupWelcomLabelUI()
+        setupCalendarViewUI()
     }
     
     private func setupWelcomLabelUI() {
@@ -38,11 +45,30 @@ class MyInfoViewController: BaseViewController {
         welcomLabel.configure(text: text, font: .largeTitle)
     }
     
+    private func setupCalendarViewUI() {
+        calendarView.configure()
+        
+        calendarBackgroundView.backgroundColor = .white
+        calendarBackgroundView.applyShadow()
+        calendarBackgroundView.applyRadius()
+    }
+    
     // MARK: - Setup Layout
     override func setupLayout() {
         welcomLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(48)
             make.leading.trailing.equalToSuperview().inset(Padding.leftRightSpacing.rawValue)
+        }
+        
+        calendarBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(welcomLabel.snp.bottom).offset(24)
+            make.leading.trailing.equalTo(welcomLabel)
+            make.height.equalTo(360)
+        }
+        
+        calendarView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(8)
         }
     }
 }

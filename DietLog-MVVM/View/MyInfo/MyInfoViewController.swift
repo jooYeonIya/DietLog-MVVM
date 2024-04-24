d592338a3827b05a11447a0ec6e496f6a47ad227
jooYeon <jooyeonstudyios@gmail.com>
2024-04-24 18:00:41 +0900

    design: 인사 및 닉네임 label 추가 (#3)

--- DietLog-MVVM/View/MyInfo/MyInfoViewController.swift
+++ DietLog-MVVM/View/MyInfo/MyInfoViewController.swift
@@ -6,10 +6,43 @@
 //
 
 import UIKit
+import FSCalendar
 
-class MyInfoViewController: UIViewController {
+enum MyInfoViewText: String {
+    case welcom = "안녕하세요"
+}
 
+class MyInfoViewController: BaseViewController {
+    
+    // MARK: - Component
+    private lazy var welcomLabel = UILabel()
+    
+    // MARK: - 변수
+    private let nickname: String = "nickname"
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayTopView(true)
+    }
+    
+    // MARK: - Setup UI
+    override func setupUI() {
+        view.addSubviews([welcomLabel])
+        setupWelcomLabelUI()
+    }
+    
+    private func setupWelcomLabelUI() {
+        let text = "\(MyInfoViewText.welcom.rawValue) \(nickname)"
+        
+        welcomLabel.configure(text: text, font: .largeTitle)
+    }
+    
+    // MARK: - Setup Layout
+    override func setupLayout() {
+        welcomLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(48)
+            make.leading.trailing.equalToSuperview().inset(Padding.leftRightSpacing.rawValue)
+        }
     }
 }

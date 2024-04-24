848906009e2b4c323fa1de5c9e57bea13d27a00a
jooYeon <jooyeonstudyios@gmail.com>
2024-04-24 15:55:23 +0900

    design: UILabel configure 추가 (#1)

---
+++ DietLog-MVVM/Global/Extension/UILable+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UILable+Extension.swift
+//  DietLog-MVVM
+//
+//  Created by Jooyeon Kang on 2024/04/24.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    func configure(text: String, font: UIFont) {
+        self.text = text
+        self.font = font
+    }
+}

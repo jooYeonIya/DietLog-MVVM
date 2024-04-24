0aaccbb2844ee59a2cd42c131c30b2607f9ffaf1
jooYeon <jooyeonstudyios@gmail.com>
2024-04-25 03:53:38 +0900

    design: CalendarView 추가 (#7)

--- DietLog-MVVM/View/Meal/MealViewController.swift
+++ DietLog-MVVM/View/Meal/MealViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import FSCalendar
 
 class MealViewController: BaseViewController {
+    
+    // MARK: - Component
+    private lazy var calendarView = FSCalendar()
+    private lazy var calendarBackgroundView = UIView()
 
     // MARK: - Life Cycle
     override func viewDidLoad() {
@@ -15,5 +20,33 @@ class MealViewController: BaseViewController {
         displayTopView(true)
     }
     
+    // MARK: - Setup UI
+    override func setupUI() {
+        view.addSubviews([calendarBackgroundView])
+        
+        setupCalendarViewUI()
+    }
     
+    private func setupCalendarViewUI() {
+        calendarView.configure()
+        
+        calendarBackgroundView.addSubview(calendarView)
+        calendarBackgroundView.backgroundColor = .white
+        calendarBackgroundView.applyRadius()
+        calendarBackgroundView.applyShadow()
+    }
+    
+    // MARK: - Setup Layout
+    override func setupLayout() {
+        calendarBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(24)
+            make.leading.trailing.equalToSuperview().inset(Padding.leftRightSpacing.rawValue)
+            make.height.equalTo(360)
+        }
+        
+        calendarView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(8)
+        }
+    }
 }
